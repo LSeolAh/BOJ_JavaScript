5793a4757016d269633dec8f5f814a06b35081d7
LSeolAh <sualah0417@gmail.com>
2022-08-29 00:49:05 +0900

    no.1000

---
+++ no.1000.js
@@ -0,0 +1,29 @@
+//실행할때는 bash창에 node [file이름]
+
+//라이브러리 가져오는 너낌
+const rl = require("readline").createInterface ({
+    input: process.stdin, 
+    output: process.stdout,
+})
+
+let input = [];
+
+//입력 받는 곳
+rl.on("line", (line) => {
+    input = line.split(" ").map((el) => Number(el));
+    rl.close(); //안하면 계속 입력 받음
+})
+
+//출력하는 곳
+rl.on("close", () => {
+    console.log(input[0] + input[1]);
+    process.exit();
+})
+
+
+// const input = require("fs").readFileSync(0).toString().trim().map((el) => Number(el));
+
+// const A = input[0];
+// const B = input[1];
+
+// console.log(A + B);
