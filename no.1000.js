70062f0c9ed51c602bf30a7caf680b42e648d653
LSeolAh <sualah0417@gmail.com>
2022-08-30 22:14:05 +0900

    no.1000

--- no.1000.js
+++ no.1000.js
@@ -1,29 +1,9 @@
-//실행할때는 bash창에 node [file이름]
+const file =
+process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 
-//라이브러리 가져오는 너낌
-const rl = require("readline").createInterface ({
-    input: process.stdin, 
-    output: process.stdout,
-})
+const input = require("fs").readFileSync(file).toString().trim().split(" ");
 
-let input = [];
+const A = Number(input[0]);
+const B = Number(input[1]);
 
-//입력 받는 곳
-rl.on("line", (line) => {
-    input = line.split(" ").map((el) => Number(el));
-    rl.close(); //안하면 계속 입력 받음
-})
-
-//출력하는 곳
-rl.on("close", () => {
-    console.log(input[0] + input[1]);
-    process.exit();
-})
-
-//좀 더 해봐야함
-// const input = require("fs").readFileSync(0).toString().trim().map((el) => Number(el));
-
-// const A = input[0];
-// const B = input[1];
-
-// console.log(A + B);
+console.log(A + B);
