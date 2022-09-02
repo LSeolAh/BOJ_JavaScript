31b0c3d1381243cd1e65fccf7f6f4b1040dd165e
LSeolAh <sualah0417@gmail.com>
2022-09-02 15:33:12 +0900

    입출력과 사칙연산

--- no.10718.js
+++
@@ -1,4 +0,0 @@
-const army = "강한친구 대한육군";
-
-console.log(army);
-console.log(army);
--- no.10926.js
+++
@@ -1,8 +0,0 @@
-const file = 
-process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-
-const input = require("fs").readFileSync(file).toString().trim();
-
-const A = (input);
-
-console.log(input + "??!");
