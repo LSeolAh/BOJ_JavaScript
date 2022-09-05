886aabb11197fb96cf8637b905cbb260ec856775
LSeolAh <sualah0417@gmail.com>
2022-09-05 17:33:54 +0900

    no.25083

---
+++ Level.1/no.25083.js
@@ -0,0 +1,7 @@
+//세번째 로그는 문자열에 큰따옴표랑 작은따옴표가 들어가있어서 백틱 처리
+console.log("         ,r'\"7");
+console.log("r`-_   ,'  ,/");
+console.log(` \\. ". L_r'`);
+console.log("   `~\\/");
+console.log("      |");
+console.log("      |");
