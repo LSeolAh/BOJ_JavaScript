6e6fe481585a4fb7155b6b99334d6a1b01c5d54d
LSeolAh <sualah0417@gmail.com>
2022-09-05 16:28:45 +0900

    no.10172

--- Level.1/no.10172.js
+++ Level.1/no.10172.js
@@ -1,10 +1,12 @@
+//백틱(`)을 쓰면 문자열로 변환
+//백틱을 출력하고 싶으면 앞에 역슬래시
 console.log("|\\_/|");
 console.log("|q p|   /}");
 console.log(`( 0 )"""\\`);
 console.log(`|"^"\`    |`);
 console.log("||_/=\\\\__|");
 
-//
+//큰따옴표를 작은 따옴표로 감싸기
 console.log("|\\_/|");
 console.log("|q p|   /}");
 console.log('( 0 )"""\\');
