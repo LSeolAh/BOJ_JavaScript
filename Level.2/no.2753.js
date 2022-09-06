cd487ea282634ddd8798093f047e49bf67c133b7
LSeolAh <sualah0417@gmail.com>
2022-09-06 17:26:27 +0900

    윤년

--- Level.2/no.2753.js
+++ Level.2/no.2753.js
@@ -1,15 +1,11 @@
-// | -> or 연산자
-// & -> and 연산자
-
-const file =
+const file = 
 process.platform === "linux" ? "dev/stdin" : "./input.txt";
 
 const input = require("fs").readFileSync(file).toString().trim();
 
 const A = Number(input);
 
-if((A % 100 != 0) | (A % 400 == 0))
-    if(A % 4 == 0)
-        console.log("1");
-    else
-        console.log("0");
+if((A % 4 === 0) && (A % 100 !==0) || (A % 400 === 0))
+    console.log("1");
+else
+    console.log("0");
