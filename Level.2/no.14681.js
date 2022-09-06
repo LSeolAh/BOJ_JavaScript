3df7e393cb5f8dc78ee0f46dd7955d4fc5d1fc57
LSeolAh <sualah0417@gmail.com>
2022-09-07 01:22:17 +0900

    사분면 고르기

--- Level.2/no.14681.js
+++ Level.2/no.14681.js
@@ -1,12 +1,45 @@
+// const file =
+// process.platform === "linux" ? "dev/stdin" : "./input.txt";
+
+// const input = require("fs").readFileSync(file).toString().trim().split("\n");
+
+// const A = Number(input[0]);
+// const B = Number(input[1]);
+
+// if((A > 0) && (B > 0)) console.log("1");
+// else if((A < 0) && (B > 0)) console.log("2");
+// else if((A < 0) && (B < 0)) console.log("3");
+// else console.log("4");
+
+
+// 런타임에러
+// const file =
+// process.platform === "linux" ? "dev/stdin" : "./input.txt";
+
+// const input = require("fs").readFileSync(file).toString().trim().split("\n");
+
+// const A = Number(input[0]);
+// const B = Number(input[1]);
+
+// if (A > 0) {
+//     B > 0 ? console.log("1") : console.log("4");
+//   } else if (A < 0) {
+//     B > 0 ? console.log("2") : console.log("3");
+//   }
+
+
+//readFileSync에 file을 넣으면 런타임 에러가 나는 경우가 있음
+//그럴 땐 file을 0으로 바꿔주거나 "dev/stdin"을 0으로 바꿔주기
 const file =
-process.platform === "linux" ? "dev/stdin" : "./input.txt";
+process.platform === "linux" ? 0 : "./input.txt";
 
 const input = require("fs").readFileSync(file).toString().trim().split("\n");
 
 const A = Number(input[0]);
 const B = Number(input[1]);
 
-if((A > 0) && (B > 0)) console.log("1");
-else if((A < 0) && (B > 0)) console.log("2");
-else if((A < 0) && (B < 0)) console.log("3");
-else console.log("4");
+if (A > 0) {
+    B > 0 ? console.log("1") : console.log("4");
+  } else if (A < 0) {
+    B > 0 ? console.log("2") : console.log("3");
+  }
