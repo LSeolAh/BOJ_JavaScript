911180f8e67fe6baeabff79ae0dadd405e3cf33e
LSeolAh <sualah0417@gmail.com>
2022-09-06 17:34:13 +0900

    윤년

--- Level.2/no.2753.js
+++ Level.2/no.2753.js
@@ -1,3 +1,4 @@
+//if문 두개로 풀기
 const file = 
 process.platform === "linux" ? "dev/stdin" : "./input.txt";
 
@@ -5,7 +6,24 @@ const input = require("fs").readFileSync(file).toString().trim();
 
 const A = Number(input);
 
-if((A % 4 === 0) && (A % 100 !==0) || (A % 400 === 0))
+if (A % 4 === 0) {
+    if((A % 100 !==0) || (A % 400 === 0))
+        console.log("1");
+    else
+        console.log("0");
+}
+else
+    console.log("0");
+
+//if문 하나로 풀기
+const f = 
+process.platform === "linux" ? "dev/stdin" : "./input.txt";
+
+const inp = require("fs").readFileSync(f).toString().trim();
+
+const a = Number(inp);
+
+if((a % 4 === 0) && (a % 100 !==0) || (a % 400 === 0))
     console.log("1");
 else
     console.log("0");
