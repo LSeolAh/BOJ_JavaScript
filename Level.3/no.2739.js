79b1659ce12dd1b0e06a42a4aed3fd6ceb725633
LSeolAh <sualah0417@gmail.com>
2022-09-11 20:52:39 +0900

    구구단

--- Level.3/no.2739.js
+++ Level.3/no.2739.js
@@ -5,4 +5,4 @@ const input = require("fs").readFileSync(file).toString().trim().split(" ");
 
 let num = Number(input[0]);
 
-for (let i=1; i<=9; i++) console.log(i * num);
+for (let i=1; i<=9; i++) console.log(num + " * " + i + " = " + i*num);
