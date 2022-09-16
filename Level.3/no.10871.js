9042d9b417d3244dc9e48607be79c5c5e3c5397d
LSeolAh <sualah0417@gmail.com>
2022-09-17 01:09:50 +0900

    X보다 작은 수

--- Level.3/no.10871.js
+++ Level.3/no.10871.js
@@ -2,8 +2,9 @@ const file =
 process.platform === "linux" ? 0 : "./input.txt";
 
 const input = require("fs").readFileSync(file).toString().trim().split("\n");
-let N = Number(input[0].split(" ")[0]);
-let X = Number(input[0].split(" ")[1]);
-let arr = input[1].split(" ").map((el) => Number(el));
+
+let [N,X] = (input[0].split(" ").map((el) => +(el)));
+
+let arr = input[1].split(" ").map((el) => +(el));
 
 console.log(arr.filter(answer => answer < X).join(" "));
