bfd78a864963b3c87a2e210c0948aaf97db01e3f
LSeolAh <sualah0417@gmail.com>
2022-09-19 23:22:05 +0900

    더하기 사이클

--- Level.3/no.1110.js
+++ Level.3/no.1110.js
@@ -1,6 +1,7 @@
 const file =
 process.platform === "linux" ? 0 : "./input.txt";
 
+//배열은 string으로 들어가기 때문에 숫자로 변환
 const input = Number(require("fs").readFileSync(file).toString().trim());
 
 let num = input;
@@ -8,6 +9,7 @@ let sum;
 let i = 0;
 
 while (true) {
+    //(num / 10)은 소숫점이 나오기 때문에 int형으로 변환
     sum = parseInt(num / 10) + (num % 10);
     num = (num % 10) * 10 + (sum % 10);
     i++;
