5473514f71ad0e7f555542397a00b5ff9187a10b
LSeolAh <sualah0417@gmail.com>
2022-09-22 14:16:32 +0900

    나머지(아직 안됨)

---
+++ Level.4/no.3025.js
@@ -0,0 +1,12 @@
+const file = process.platform === "linux" ? 0 : "./input.txt";
+
+const input = require("fs")
+  .readFileSync(file)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => Number(el));
+
+let array = [];
+
+for (i = 0; i <= 9; i++) array.push(input[i] % 42);
