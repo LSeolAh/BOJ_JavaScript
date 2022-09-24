53595baabb0d2f7ffc4ae8677092136878a8dc7e
LSeolAh <sualah0417@gmail.com>
2022-09-24 21:55:44 +0900

    나머지

--- Level.4/no.3025.js
+++ Level.4/no.3025.js
@@ -11,8 +11,10 @@ let array = [];
 
 for (i = 0; i <= 9; i++) array.push(input[i] % 42);
 
+//중복값이 있는 배열 Set객체로 만들어 중복 제거
 let set = new Set(array);
 
+//전개연산자를 사용하여 Set객체를 다시 배열로 변환
 let answer = [...set];
 
 console.log(answer.length);
